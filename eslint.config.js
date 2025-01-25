90aa4f8515e78a0e183292fa303279a5837cb873
Roman Tsyhulov <roman.t@devbrother.com>
2025-01-25 02:47:12 +0200

    set up/ Initialize linter

--- eslint.config.js
+++ eslint.config.js
@@ -3,22 +3,75 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import typescript from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser: typescript,
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint,
+      'import': importPlugin,
+      'unused-imports': unusedImports,
+      'prettier': prettier
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        typescript: {}
+      }
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn', 
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'import/order': ['warn', {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index']
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true }
+      }],
+      'unused-imports/no-unused-imports': 'warn',
+      'max-len': ['warn', {
+        code: 120,
+        ignoreComments: true,
+        ignoreUrls: true
+      }],
+      'no-multiple-empty-lines': ['warn', { max: 1 }],
+      'react/prop-types': 'off',
+      'react/self-closing-comp': 'warn',
+      'prettier/prettier': ['warn', {
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 120,
+        tabWidth: 2,
+        semi: true,
+        arrowParens: 'always'
+      }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
