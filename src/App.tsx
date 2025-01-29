7a3e56d2c7aa33616bec990efd93b10732500ce1
Roman Tsyhulov <roman.t@devbrother.com>
2025-01-29 03:39:14 +0200

    set up theme provider

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "./components/theme/ThemeProvider"
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export default App
